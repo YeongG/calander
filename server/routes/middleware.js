f5225ab978aed2a3bac412b204d08f3c8b8f2a38
YeongG <yeonggil223@gmail.com>
2020-12-06 23:25:59 +0900

    [CREATE] middleware 정의

---
+++ server/routes/middleware.js
@@ -0,0 +1,16 @@
+const { checkJWT } = require("../lib/func");
+
+const isLogined = async (req, res, next) => {
+  const token = req.headers.authorization;
+  try {
+    const res = await checkJWT(token);
+    req.decode = { ...res };
+    next();
+  } catch (err) {
+    res.status(409).json({ message: "토큰이 이상해요" });
+  }
+};
+
+module.exports = {
+  isLogined,
+};
